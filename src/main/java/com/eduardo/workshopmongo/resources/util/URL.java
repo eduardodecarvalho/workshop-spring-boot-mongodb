62b98b512956776d455b7cac163d805c94331377
eduardo_decarvalho <eduardobcarvalho@outlook.com>
2019-03-17 13:40:49 -0300

    Consulta simples com @Query

--- src/main/java/com/eduardo/workshopmongo/resources/util/URL.java
+++ src/main/java/com/eduardo/workshopmongo/resources/util/URL.java
@@ -4,8 +4,9 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 public class URL {
-	
+
 	public static String decodeParam(String text) {
+
 		try {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
